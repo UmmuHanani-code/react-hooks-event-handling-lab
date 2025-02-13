acd158462638eccb5334667dea629e40c2074c35
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-02-13 17:53:17 +0300

    commit changes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad () {
+
+      function handleChange() {
+        return console.log("Entering password...");
+      }
     return (
-        <div></div>
-    )
+        <div>
+            <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="Enter Password"
+
+            />
+        </div>
+    );
 }
 
 export default Keypad;
